5ace481461dc98b45631a0d5c7072576bdbbc5ce
Makayla Bryan <makaylabryan@gmail.com>
2023-05-03 16:26:03 -0400

    tests created for palindrome

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,20 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("flowers", false)]
+        [InlineData("civic", true)]
+        [InlineData("civil", false)]
+        [InlineData("redivider", true)]
+        [InlineData("zoo", false)]
+        public void Test1(string word, bool expected)
         {
+            WordSmith testWord = new WordSmith();
 
+            bool actual = testWord.IsAPalindrome(word);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
